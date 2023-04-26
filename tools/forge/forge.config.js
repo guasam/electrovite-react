67c32efe5dbda0f76bf94e7461a974b743273290
guasam <guasamos@outlook.com>
2023-04-26 14:57:38 +0530

    Create forge config file

---
+++ tools/forge/forge.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const rootDir = process.cwd();
+
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-vite',
+      config: {
+        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+        // If you are familiar with Vite configuration, it will look really familiar.
+        build: [
+          {
+            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+            entry: path.join(rootDir, 'src/main/main.ts'),
+            config: path.join(rootDir, 'tools/vite/vite.main.config.ts'),
+          },
+          {
+            entry: path.join(rootDir, 'src/renderer/preload.ts'),
+            config: path.join(rootDir, 'tools/vite/vite.preload.config.ts'),
+          },
+        ],
+        renderer: [
+          {
+            // Name becomes available in main/main.ts in capital case format: 'MAIN_WINDOW_**'
+            name: 'main_window',
+            config: path.join(rootDir, 'tools/vite/vite.renderer.config.ts'),
+          },
+        ],
+      },
+    },
+  ],
+};
