39b57c37606465d2d365cc5e5a2326b03a8beb3f
guasam <guasamos@outlook.com>
2023-04-26 15:55:41 +0530

    Impl:  Renderer with default app layout & images

---
+++ src/renderer/components/App.tsx
@@ -0,0 +1,10 @@
+import image from '../../../assets/electrovite.png';
+
+const App = () => {
+  return <div>This is an app heress.
+
+    <img src={image} />
+  </div>;
+};
+
+export default App;
---
+++ src/renderer/renderer.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import './scss/app.scss';
+
+// Say something
+console.log('[EVite] : renderer executed');
+
+// Render application
+createRoot(document.getElementById('app') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
