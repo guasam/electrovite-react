6886936bfb4f7fcfb1959c13dcd8bd8a42087599
guasam <guasamos@outlook.com>
2023-04-26 17:19:10 +0530

    Reformat

--- src/main/main.ts
+++ src/main/main.ts
@@ -76,7 +76,9 @@ function createMainWindow() {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+    );
   }
 
   // Show window when its ready to
